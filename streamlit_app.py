5c5ea2866c5a8dbfeaf2d660fcc66a47c855d451
Stanislava Slavova <108274746+sslavova@users.noreply.github.com>
2022-07-01 17:19:15 +0300

    Update streamlit_app.py
    add additional table output

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#New Section to display fruityvice api response
+# New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
