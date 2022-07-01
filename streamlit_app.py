658256fc6b3ff0a498e181fae15f88c64fe86f7e
Stanislava Slavova <108274746+sslavova@users.noreply.github.com>
2022-07-01 23:33:47 +0300

    Update streamlit_app.py
    turned back to text single result

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
-streamlit.data_frame(my_data_row)
+streamlit.text(my_data_row)
 
